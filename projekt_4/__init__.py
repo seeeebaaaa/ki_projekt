499d0454777261ae7f049515694eed611630e891
seeeebaaaa <>
2025-03-26 10:21:48 +0100

    added simple module

---
+++ projekt_4/__init__.py
@@ -0,0 +1,2 @@
+# import modules to improt directly from projekt_4
+from .hello_world import hello, world, add_hello
---
+++ projekt_4/hello_world.py
@@ -0,0 +1,8 @@
+def hello():
+    print("Hello world :D")
+
+def world():
+    print("The whole world")
+
+def add_hello(input):
+    return input+", hello!"
